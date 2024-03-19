9e08b530ff88a7c820d443e9f40a886516d03cb1
Karin Patenge <karin.patenge@oracle.com>
2024-03-19 10:59:18 +0100

    Folder moved into a different repo.

--- scripts/IT Days 365 in 2024/16_query_graph_metadata.sql
+++
@@ -1,8 +0,0 @@
-------------------------------------------
--- Query the Property Graph metadata views
-------------------------------------------
-
-select * from user_property_graphs;
-select * from user_pg_elements where graph_name='FLIGHT_GRAPH';
-select * from user_pg_label_properties where graph_name='FLIGHT_GRAPH';
-select * from user_pg_labels where graph_name='FLIGHT_GRAPH';
--- scripts/IT Days 365 in 2024/README.md
+++
@@ -1,3 +0,0 @@
-# General remarks
-
-This folder contains scripts created for a presentation with live demo during [IT-Tage 365 - Thementage Remote - Datenbank Tage](https://www.ittage.informatik-aktuell.de/thementage.html#c26475) on February 28, 2024.
